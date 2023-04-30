57c9411dbc0d398df472a5ce6da26b27efe19237
shio <85730998+dino3616@users.noreply.github.com>
2023-04-30 12:25:27 +0000

    feat: ✨ (error): error モジュールを作成

--- src/module/error/error.c
+++ src/module/error/error.c
@@ -1,15 +1,15 @@
-#include "lib/error.h"
+#include "module/error/error.h"
 
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-void exit_as_error(const char *format, ...) {
+void print_error(const char *format, ...) {
   va_list args;
 
   va_start(args, format);
+  fprintf(stderr, "\033[31m");
+  fprintf(stderr, "Error: ");
   vfprintf(stderr, format, args);
+  fprintf(stderr, "\033[0m\n");
   va_end(args);
-
-  exit(EXIT_FAILURE);
 }
---
+++ src/module/error/error.h
@@ -0,0 +1,6 @@
+#ifndef MODULE_ERROR_ERROR_H
+#define MODULE_ERROR_ERROR_H
+
+extern void print_error(const char *format, ...);
+
+#endif
