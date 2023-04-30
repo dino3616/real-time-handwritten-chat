e681d7b0718fbe71d3b74c6fb530d2d25cf068d1
shio <85730998+dino3616@users.noreply.github.com>
2023-04-30 12:28:20 +0000

    feat: ✨ (command): help_command モジュールを作成

---
+++ src/module/command/help_command.c
@@ -0,0 +1,13 @@
+#include "module/command/help_command.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int help_command() {
+  printf("Available commands:\n");
+  printf("  c - Change the pen color.\n");
+  printf("  q - Quit the program.\n");
+  printf("  h - Display this help message.\n");
+
+  return EXIT_SUCCESS;
+}
---
+++ src/module/command/help_command.h
@@ -0,0 +1,6 @@
+#ifndef MODULE_COMMAND_HELP_H
+#define MODULE_COMMAND_HELP_H
+
+extern int help_command();
+
+#endif
