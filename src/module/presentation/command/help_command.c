c16983485e5ee6b0be131b9f09f38a7684bf8acd
shio <85730998+dino3616@users.noreply.github.com>
2023-05-16 04:59:46 +0000

    feat: 🎈 (command) help コマンドに message コマンドを追加

--- src/module/presentation/command/help_command.c
+++ src/module/presentation/command/help_command.c
@@ -6,8 +6,9 @@
 int help_command() {
   printf("Available commands:\n");
   printf("  c - Change the pen color.\n");
-  printf("  q - Quit the program.\n");
   printf("  h - Display this help message.\n");
+  printf("  m - Send a message to other clients.\n");
+  printf("  q - Quit the program.\n");
 
   return EXIT_SUCCESS;
 }
