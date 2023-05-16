e74d88e8e580c17039603f65fa42b6fbf458d070
shio <85730998+dino3616@users.noreply.github.com>
2023-05-16 04:56:52 +0000

    feat: 🎈 (context) message context を追加

--- src/core/context/context.h
+++ src/core/context/context.h
@@ -3,13 +3,30 @@
 
 #include "core/util/color.h"
 
+#define GET_REQUEST_METHOD 0
+#define POST_REQUEST_METHOD 1
+
 #define PAINTED_EVENT 1
 #define EXPOSE_EVENT 2
 
+#define MESSAGE_LENGTH 128
+#define MESSAGE_HISTORY_SIZE 128
 #define PAINT_HISTORY_SIZE 1024
+
+typedef struct MessageHistory {
+  int client_id;
+  RGBColor_t rgb_color;
+  char message[MESSAGE_LENGTH];
+} MessageHistory_t;
+
+typedef struct MessageContext {
+  MessageHistory_t all_message_histories[MESSAGE_HISTORY_SIZE];
+  MessageHistory_t additional_message_history;
+} MessageContext_t;
 
 typedef struct CommandContext {
   char command;
+  MessageContext_t message_context;
 } CommandContext_t;
 
 typedef struct Point {
@@ -30,6 +47,7 @@ typedef struct EventContext {
 } EventContext_t;
 
 typedef struct SocketContext {
+  int request_method;
   CommandContext_t command_context;
   EventContext_t event_context;
 } SocketContext_t;
