a2f62588afc45377d2f9a729c91021b914b6acbe
shio <85730998+dino3616@users.noreply.github.com>
2023-05-16 04:59:20 +0000

    refactor: ♻️ (command) quit の処理を quit_command に集約するように修正

--- src/module/presentation/command/quit_command.c
+++ src/module/presentation/command/quit_command.c
@@ -4,11 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 
+#include "core/context/context.h"
 #include "core/log/log.h"
 
-int quit_command() {
+int quit_command(int socket_fd, SocketContext_t *socket_context) {
   printf("Bye-bye :)\n");
+
+  socket_context->request_method = POST_REQUEST_METHOD;
+  if (send(socket_fd, socket_context, sizeof(SocketContext_t), 0) < 0) {
+    int error_number = errno;
+    log_error("Failed to send socket context to server. cause: '%s'",
+              strerror(error_number));
+
+    return EXIT_FAILURE;
+  }
 
   exit(EXIT_SUCCESS);
 
--- src/module/presentation/command/quit_command.h
+++ src/module/presentation/command/quit_command.h
@@ -1,6 +1,8 @@
 #ifndef MODULE_PRESENTATION_COMMAND_QUIT_COMMAND_H
 #define MODULE_PRESENTATION_COMMAND_QUIT_COMMAND_H
 
-extern int quit_command();
+#include "core/context/context.h"
+
+extern int quit_command(int socket_fd, SocketContext_t *socket_context);
 
 #endif
